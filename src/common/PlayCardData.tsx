41a9575f0b318d3d55bfd3ea364c140b3d43cfe9
akashsoni0786 <akashsoni.cedcoss@gmail.com>
2024-03-09 12:41:28 +0530

    Images added

--- src/common/PlayCardData.tsx
+++ src/common/PlayCardData.tsx
@@ -6,7 +6,7 @@ export const cardData = [
     disable: false,
     id: "111",
     match: "111",
-    img: "https://drive.google.com/file/d/1zTTjitiDimHNOLcx9qIZsrrsiYsxNcss/view?usp=drive_link",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsexk0nun04CtaLl4NGT2WKBzwZOpSis5daw&usqp=CAU", 
   },
   {
     rotate: false,
@@ -15,7 +15,7 @@ export const cardData = [
     disable: false,
     id: "222",
     match: "222",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE3YAfczidYzm7iTUvvxShmIfJ0JwT554ZLg&usqp=CAU",
+    img: "https://i.pinimg.com/736x/86/51/18/8651189fb9074a06c62e35187d7270d0.jpg", 
   },
 
   {
@@ -25,7 +25,7 @@ export const cardData = [
     disable: false,
     id: "333",
     match: "333",
-    img: "https://i.pinimg.com/736x/8d/c3/e2/8dc3e2cd442257d9e450b4b74b7f91ac.jpg",
+    img: "https://www.freebiefindingmom.com/wp-content/uploads/2021/02/free_printable_colorful_cartoon_letters_cartoon_letter_A-1-683x1024.jpg", 
   },
 
   {
@@ -35,7 +35,7 @@ export const cardData = [
     disable: false,
     id: "444",
     match: "444",
-    img: "https://rukminim1.flixcart.com/image/416/416/kph8h3k0/poster/e/w/4/large-adorable-cat-poster-cute-kittens-poster-cat-poster-funny-original-imag3p7tcxuzhpn2.jpeg?q=70",
+    img: "https://i.pinimg.com/736x/94/3d/f5/943df5e4623fa36f90e0d3c3142dca55.jpg", 
   },
 
   {
@@ -45,7 +45,7 @@ export const cardData = [
     disable: false,
     id: "555",
     match: "555",
-    img: "https://cdn2.sharechat.com/CuteRabbit.._11a4c6a6_1643787742842_sc_exp_cmprsd_75.jpg",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxe12u3ZZ6ojMiiafmAs4fduq4pGrnH2Iuug&usqp=CAU",
   },
 
   {
@@ -55,7 +55,7 @@ export const cardData = [
     disable: false,
     id: "666",
     match: "666",
-    img: "https://i.etsystatic.com/16945464/r/il/94aaec/2962199690/il_fullxfull.2962199690_l54p.jpg",
+    img: "https://img.freepik.com/premium-vector/letter-b-with-banana-cartoon-illustration_670095-429.jpg", //B
   },
   {
     rotate: false,
@@ -64,7 +64,7 @@ export const cardData = [
     disable: true,
     id: "1111",
     match: "111",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbNZg1jcN9vbQMLED6sUPXAR9f7bidtoyfMg&usqp=CAU",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLyExh3d4yr_1cSq_Eo9nSr0VpFlIzuKl1lA&usqp=CAU", //Strawberry
   },
   {
     rotate: false,
@@ -73,7 +73,7 @@ export const cardData = [
     disable: true,
     id: "2222",
     match: "222",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE3YAfczidYzm7iTUvvxShmIfJ0JwT554ZLg&usqp=CAU",
+    img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqO18vnrtNNaUC1ITlnn6ymfnV1n_GM2rebQ&usqp=CAU", //Orange
   },
 
   {
@@ -83,7 +83,7 @@ export const cardData = [
     disable: true,
     id: "3333",
     match: "333",
-    img: "https://i.pinimg.com/736x/8d/c3/e2/8dc3e2cd442257d9e450b4b74b7f91ac.jpg",
+    img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL2A6qvMZCFLr2exB_NsFNn9q9kXD9tYtOdg&usqp=CAU", //Apple
   },
 
   {
@@ -93,7 +93,7 @@ export const cardData = [
     disable: true,
     id: "4444",
     match: "444",
-    img: "https://rukminim1.flixcart.com/image/416/416/kph8h3k0/poster/e/w/4/large-adorable-cat-poster-cute-kittens-poster-cat-poster-funny-original-imag3p7tcxuzhpn2.jpeg?q=70",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMD7YTbSnHlprDlBnsxuZX6cNpSduyNfOSLQ&usqp=CAU", //Watermelon
   },
 
   {
@@ -103,7 +103,7 @@ export const cardData = [
     disable: true,
     id: "5555",
     match: "555",
-    img: "https://cdn2.sharechat.com/CuteRabbit.._11a4c6a6_1643787742842_sc_exp_cmprsd_75.jpg",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZqfhYdduE2gHJzurSfYc3X8s8RX4sA5N4Kw&usqp=CAU", // p
   },
 
   {
@@ -114,6 +114,6 @@ export const cardData = [
     id: "6666",
     match: "666",
 
-    img: "https://i.etsystatic.com/16945464/r/il/94aaec/2962199690/il_fullxfull.2962199690_l54p.jpg",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3-Un5L9NDyl3JOfu__7IeGAg4a5vUPusZDQ&usqp=CAU", //B
   },
 ];
